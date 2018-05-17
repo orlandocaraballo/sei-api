15b5d5ce8439a0100936b51846c11795e78240a3
Orlando Caraballo <orlandocaraballo@gmail.com>
2018-05-17 00:16:53 -0400

    Added new utils into app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const fs = require('fs')
+const utils = require('./utils')
 
 const app = express()
+const studentsJSON = JSON.parse(fs.readFileSync('students.json', 'utf8'))
 
-app.get("/", (request, response) => {
-  response.json({ name : "orlando" })
+app.get("/students", (request, response) => {
+  response.json(studentsJSON)
+})
+
+app.get("/students/random", (request, response) => {
+  response.json(studentsJSON[utils.random(0, 9)])
+})
+
+app.get("/students/:id", (request, response) => {
+  console.log(utils.random(1,10))
+  response.json(studentsJSON[request.params['id'] - 1])
 })
 
 app.listen(3000, () => {

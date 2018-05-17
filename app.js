e5e6901340b785c82fff5ad8342029a2cc57effc
Orlando Caraballo <orlandocaraballo@gmail.com>
2018-05-17 01:15:10 -0400

    Added new files

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+// require our packages
 const express = require('express')
 const fs = require('fs')
+const ejs = require('ejs')
 const utils = require('./utils')
 
+// setup our app
 const app = express()
 const studentsJSON = JSON.parse(fs.readFileSync('students.json', 'utf8'))
 const port = process.env.PORT || 3000
+const indexTemplate = ejs.compile(fs.readFileSync('views/index.ejs', 'utf8'))
+
+// allows us to serve all files within public directory
+app.use(express.static('public'))
+
+// set our routes
+app.get("/", (request, response) => {
+  response.end(indexTemplate({ 
+    title: "The title",
+    students: studentsJSON
+  }))
+})
 
 app.get("/students", (request, response) => {
   response.json(studentsJSON)

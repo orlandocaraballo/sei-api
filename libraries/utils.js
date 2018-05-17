b355ea6326009df313271feab9d7bb5217f6e2a3
orlandocaraballo <orlando.caraballo@nycda.com>
2018-05-17 17:28:39 -0400

    Created loadJSON util function Removed references to readSync regarding JSON Closes #8

--- libraries/utils.js
+++ libraries/utils.js
@@ -1,5 +1,17 @@
+const fs = require('fs')
+
+// picks random number between min and max
 module.exports.random = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min
+}
+
+// loads json file from filepath then executes callback
+module.exports.loadJSON = (filepath, callback) => {
+  fs.readFile(filepath, (error, data) => {
+    if (error) throw error
+    
+    callback(JSON.parse(data))
+  })
 }

a6ab6303158fc2d771e8251643fbda5d5d8cdb42
Orlando Caraballo <orlandocaraballo@gmail.com>
2018-09-03 18:40:00 -0400

    Fixed typo

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
     // TODO: add ssl certificate
     // leaving this out until I understand it lol

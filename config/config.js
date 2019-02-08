10a230ec46c3cc0c00e65e7f745aa9bf5684cf82
Orlando Caraballo <orlandocaraballo@gmail.com>
2019-02-08 18:08:41 -0500

    Removed test environment

--- config/config.js
+++ config/config.js
@@ -8,13 +8,6 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'postgres'
   },
-  test: {
-    username: 'database_test',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'

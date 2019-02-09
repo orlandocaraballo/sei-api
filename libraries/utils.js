8381c126cb717bb449bec3548ca8cc3f07ba1ad0
Orlando Caraballo <orlandocaraballo@gmail.com>
2019-02-09 08:38:51 -0500

    Made changes to utils.js file

--- libraries/utils.js
+++ libraries/utils.js
@@ -8,10 +8,12 @@ module.exports.APP_PATH = path.join(__dirname, '..');
 // set cohort names
 module.exports.FIRST_COHORT_NAME = 'edge-case';
 module.exports.SECOND_COHORT_NAME = 'methods';
+module.exports.THIRD_COHORT_NAME = 'sei-nov';
 
 // sets json path of students
 module.exports.FIRST_COHORT_JSON_PATH = path.join(__dirname, '..', 'data', `${ this.FIRST_COHORT_NAME }.json`);
 module.exports.SECOND_COHORT_JSON_PATH = path.join(__dirname, '..', 'data', `${ this.SECOND_COHORT_NAME }.json`);
+module.exports.THIRD_COHORT_JSON_PATH = path.join(__dirname, '..', 'data', `${ this.THIRD_COHORT_NAME }.json`);
 
 // picks random number between min and max
 module.exports.random = (min, max) => {
@@ -47,4 +49,15 @@ module.exports.loadSecondCohortJSON = (callback) => {
 // loads "methods" json file synchronously
 module.exports.loadSecondCohortJSONSync = () => {
   return JSON.parse(fs.readFileSync(this.SECOND_COHORT_JSON_PATH, ENCODING));
+}
+
+// loads "sei-nov" json file asynchronously
+// TODO: convert to promise
+module.exports.loadThirdCohortJSON = (callback) => {
+  this.loadJSON(this.THIRD_COHORT_JSON_PATH, callback);
+}
+
+// loads "sei-nov" json file synchronously
+module.exports.loadThirdCohortJSONSync = () => {
+  return JSON.parse(fs.readFileSync(this.THIRD_COHORT_JSON_PATH, ENCODING));
 }

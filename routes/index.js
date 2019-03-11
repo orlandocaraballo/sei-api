affd001521c8b666c566b3f436e6962be21ea146
Orlando Caraballo <orlandocaraballo@gmail.com>
2019-03-11 18:12:56 -0400

    Forgot to push routes lol

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+const models = require('../models/');
+
+// utility vars
+const encoding = 'utf8';
+const Student = models.Student;
+const Cohort = models.Cohort;
+
+// set our routes
+router.get("/", (request, response) => {
+  // setup our template
+  const indexTemplate = ejs.compile(fs.readFileSync(
+    path.join(__dirname, '..', 'views', 'index.ejs'),
+    encoding
+  ));
+
+  Student
+    .all({
+      // sort ascending by id
+      order: [
+        ['id', 'ASC']
+      ],
+      include: { model: Cohort }
+    })
+    .then(students => { 
+      response.end(indexTemplate({ 
+        title: "SEI | Homepage",
+        students: students
+      }));
+    })
+    .catch(error => response.status(400).send(error));
+});
+
+module.exports = router;
